ab308e316af8b605f6e585f17efc936cdb70e0b2
Christian Maximilian <krystianmaccs@gmail.com>
2022-07-21 08:46:27 -0400

    feat: defined cache to handle retires

--- apps/users/abstractapi.py
+++ apps/users/abstractapi.py
@@ -1,12 +1,34 @@
 import os
 
 from blog_api.settings.base import env
+from blog_api.settings import base
+from django.core.cache.backends.base import DEFAULT_TIMEOUT
+from django.views.decorators .cache import cache_page
 
 import requests
+import logging
+
+logger = logging.getLogger("abstractapi")
+
+CACHE_TTL = getattr(base, 'CACHE_TTL', DEFAULT_TIMEOUT)
 
 class AbstractAPI(object):
     HOLIDAYS_API_KEY = env("HOLIDAYS_API_KEY")
     IP_GEOLOCATION_API_KEY = env("IP_GEOLOCATION_API_KEY")
+
+
+    @cache_page(CACHE_TTL)
+    def _get(self, url, max_tries=5, **params):
+        tries = 0
+        while tries < max_tries:
+            tries += 1
+            try:
+                response = requests.get(url, params=params)
+                if response.ok:
+                    return response.json()
+            except Exception as e:
+                logger.error("Failed getting response(%s): %s", url, e)
+        return None
 
 
     def get_holiday_details(self, country_code, year, month, day):
--- apps/users/factories.py
+++ apps/users/factories.py
@@ -1,6 +1,6 @@
 from faker import Faker
 from django.contrib.auth import get_user_model
-#from factory import fuzzy
+import factory
 from factory.django import DjangoModelFactory
 
 User = get_user_model()
@@ -9,8 +9,8 @@ fake = Faker()
 
 class UserFactory(DjangoModelFactory):
     first_name = fake.first_name
-    """last_name = fuzzy.FuzzyAttribute(fuzzer=fake.last_name)
-    email = fuzzy.FuzzyAttribute(fuzzer=fake.email)"""
+    last_name = fake.last_name
+    email = fake.email
 
     class Meta:
         model = User
